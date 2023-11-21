d967442ff47f56d0743a825dde8b1e13a6a4d21d
Sam Holmes <30374167+SamHolmes1@users.noreply.github.com>
2023-11-21 12:25:52 +0000

    refactor to account for feedback

--- Controller/comments.controller.js
+++ Controller/comments.controller.js
@@ -1,9 +1,17 @@
 const { queryCommentsByArticleId } = require("../Models/comments.model");
+const { checkExists } = require("../mvc.utils");
 
 exports.getCommentsById = function (req, res, next) {
-  queryCommentsByArticleId(req.params)
+  const article_id = req.params.article_id;
+
+  const promises = [
+    checkExists("articles", "article_id", article_id),
+    queryCommentsByArticleId(article_id),
+  ];
+
+  Promise.all(promises)
     .then((data) => {
-      res.status(200).send({ comments: data });
+      res.status(200).send({ comments: data[1] });
     })
     .catch(next);
 };
--- Models/comments.model.js
+++ Models/comments.model.js
@@ -1,23 +1,16 @@
 const db = require("../db/connection");
 
 exports.queryCommentsByArticleId = function (param) {
-  if (isNaN(+param.article_id)) {
+  if (isNaN(+param)) {
     // Check data type before passing to the database to keep malformed data away from the database
     return Promise.reject({ status: 400, msg: "Invalid parameter" });
   }
   return db
     .query(
       "SELECT * FROM comments WHERE article_id = $1 ORDER BY created_at DESC",
-      [param.article_id]
+      [param]
     )
     .then(({ rows }) => {
-      if (rows.length === 0) {
-        return Promise.reject({
-          status: 404,
-          msg: [],
-        });
-      } else {
-        return rows;
-      }
+      return rows;
     });
 };
